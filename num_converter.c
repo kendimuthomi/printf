8cf31b9808a468b4584dd8281bfd064aa6dfce62
kendimuthomi <kendijackline42@gmail.com>
2022-07-11 08:43:30 +0000

    Converts number and base to string

---
+++ num_converter.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * converter - converts number and base into string
+ * @num: integer input
+ * @base: input base
+ * @lowercase: flag
+ * Return: string
+ */
+char *converter(unsigned long int num, int base, int lowercase)
+{
+	static char *rep;
+	static char buff[50];
+	char *ptr;
+
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	ptr = &buff[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+	return (ptr);
+}
