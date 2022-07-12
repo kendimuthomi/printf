828e33f68e4d76f37e1221800d03c143819a5160
kendimuthomi <kendijackline42@gmail.com>
2022-07-12 04:15:02 +0000

    Tidying up

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
  * @format: format string containing characters and specifiers
  * Description: the function will call get_format_specifier()
  * that will determine which printing function to call depending
- * on the conversion specifiers 
+ * on the conversion specifiers
  * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
@@ -17,7 +17,9 @@ int _printf(const char *format, ...)
 	flags_t flags = {0, 0, 0};
 
 	register int count = 0;
+
 	va_start(args, format);
+
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
